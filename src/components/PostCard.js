a03a4445aa4cb3204fccb4127a283c4b4dcc6f7e
NurKhamid <khamid1196@gmail.com>
2021-02-26 21:15:24 +0700

    client push

---
+++ src/components/PostCard.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import { Button, Card, Icon, Label, Image} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+import MyPopup from '../util/MyPopup';
+import DeleteButton from './DeleteButton';
+
+function PostCard({ 
+    post: {body, createdAt, id, username, likeCount, commentCount, likes}
+}){
+    const { user } = useContext(AuthContext);
+    
+    return(
+        <Card fluid>
+            <Card.Content>
+                <Image
+                floated='right'
+                size='mini'
+                src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description as={Link} to={`/posts/${id}`}>{body}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <LikeButton user={user} post={{ id, likes, likeCount }}/>
+                <MyPopup content="Comment on post">
+                    <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
+                        <Button color='blue' basic>
+                            <Icon name='comments' />
+                        </Button>
+                        <Label basic color='blue' pointing='left'>
+                            {commentCount}
+                        </Label>
+                    </Button>
+                </MyPopup>
+                {user && user.username === username && <DeleteButton postId={id}/>}
+            </Card.Content>
+            </Card>
+    )
+}
+
+export default PostCard;
